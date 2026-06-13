694240ccfe03327400596770cd935a4448555724
Robert Skwiat <rob@robertskwiat.com>
2026-06-12 20:56:28 -0400

    creating gh action for ci

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Hello Hono!, testing app')
 })
+
+
 
 export default app
